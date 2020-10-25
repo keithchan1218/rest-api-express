85297b59b1eff6e27ade20ff879c42c8b9e1db5c
Keith Chan <keithchan1218@gmail.com>
2020-10-25 19:50:27 +0800

    Add POST and DELETE

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
+// for request body support JSON format (middleware)
+app.use(express.json());
 
 const transport = [
-  {name:'bus', people:50},
-  {name:'taxi', people:5},
-  {name:'walk', people:1}
+  {name:'Bus', people:50},
+  {name:'Taxi', people:5},
+  {name:'Walk', people:1}
 ];
 
 app.get('/',(req, res) => {
-  res.send('Hello World!!!');
+  res.send('Welcome to Mini World Transportation');
 });
 
 app.get('/api/transport', (req, res) => {
-  res.send(['bus', 'taxi', 'walk']);
+  res.send(transport);
 });
 
 app.get('/api/transport/:name', (req, res) => {
@@ -22,8 +24,58 @@ app.get('/api/transport/:name', (req, res) => {
   // });
   let result = transport.find(item=>item.name == req.params.name);
   if(!result)
-    res.status(404).send('Error');
+    res.status(404).send({
+      'status':'Error',
+      'returnData':'Error transportation type!'
+    });
   res.send(result)
+});
+
+// Add new transport
+app.post('/api/transport',(req,res)=>{
+
+  // validation
+  if(!req.body.name){
+    res.status(404).send({
+      'status':'Error',
+      'returnData':'Please input valid name',
+      'inputData':req.params
+    });
+    return;
+  } else if (!req.body.people){
+    res.status(404).send({
+      'status':'Error',
+      'returnData':'Please input valid people'
+    });
+    return;
+  }
+
+  let newTransportation = {
+    name:req.body.name,
+    people:req.body.people
+  }
+  transport.push(newTransportation);
+  res.send({
+    'status':'Success',
+    'returnData':'Added new transportation'
+  });
+
+});
+
+app.delete('/api/transport/:name',(req,res)=>{
+  transport.find((item,index)=>{
+    if(item.name === req.params.name){
+      transport.splice(index,1)
+      res.send({
+        'status':'Success',
+        'returnData':'Deleted '+req.params.name
+      });
+    }
+  });
+  res.status(404).send({
+    'status':'Error',
+    'returnData':'Please input valid transportation'
+  });
 });
 
 const port = process.env.PORT || 3000;
