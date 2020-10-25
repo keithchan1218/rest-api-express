bc53fe60ec126be6a6b14faff6b791fab0d049d7
Keith Chan <keithchan1218@gmail.com>
2020-10-25 11:21:00 +0800

    Add nodemon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# rest-api-express
+# REST API using Express
 
 ## Setup
 ```
@@ -12,3 +12,14 @@ node index.js
 
 ### Use Postman to test the API
 https://www.postman.com/
+
+#### Useful Links
+https://medium.com/@brianwu291/learn-basic-node-part2-building-restful-api-by-using-express-d9fefbeefa41
+- step-by-step tutorial
+
+#### Nodemon
+Auto-run after save the file (e.g. index.js)
+nodemon index.js
+
+#### find()
+Array.prototype.find()
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
+
+const transport = [
+  {name:'bus', people:50},
+  {name:'taxi', people:5},
+  {name:'walk', people:1}
+];
+
 app.get('/',(req, res) => {
   res.send('Hello World!!!');
 });
-app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3]);
+
+app.get('/api/transport', (req, res) => {
+  res.send(['bus', 'taxi', 'walk']);
 });
+
+app.get('/api/transport/:name', (req, res) => {
+  // transport.find((item,index)=>{
+  //   if(item.name == req.params.name)
+  //   res.send(transport[index]);
+  // });
+  let result = transport.find(item=>item.name == req.params.name);
+  if(!result)
+    res.status(404).send('Error');
+  res.send(result)
+});
+
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
